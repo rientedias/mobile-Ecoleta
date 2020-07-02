be4277cf5142cb040df865232a0e8b73f4f4ebf9
rientedias <ederrientedias@hotmail.com>
2020-07-02 16:38:13 -0300

    criação da tela Points

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -1,22 +1,104 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
- const Points = () => {
+import Constants from 'expo-constants';
+import { Feather as Icon } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { View, StyleSheet, Text, TouchableOpacity, ScrollView, Image } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { SvgUri, } from 'react-native-svg';
+
+const Points = () => {
+  const navigation = useNavigation();
+
+  // Voltando para  tela Home
+  function handlenavigateBack() {
+    navigation.goBack();
+  }
+
+  // Indo para tela Detail
+  function handleNavigationToDetail(){
+    navigation.navigate('Detail');
+  }
 
   return (
-  <View style={styles.container}>
-    <Text>Points</Text>
-  </View>
-  );
- }
 
- export default Points;
+    <>
+      <View style={styles.container}>
+
+        <TouchableOpacity onPress={handlenavigateBack}>
+          <Icon name="arrow-left" size={20} color="#34cb79" />
+
+        </TouchableOpacity>
+        <Text style={styles.title}>Bem vindo</Text>
+        <Text style={styles.description}>Encontre no mapa um ponto de coleta</Text>
+
+        {/* container do Map */}
+        <View style={styles.mapContainer}>
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: -23.5359602,
+              longitude: -46.3528733,
+              latitudeDelta: 0.014,
+              longitudeDelta: 0.014
+            }}
+          >
+            <Marker
+              style={styles.mapMarker}
+              onPress={handleNavigationToDetail}
+              coordinate={{
+                latitude: -23.5359602,
+                longitude: -46.3528733,
+              }}
+            >
+              <View style={styles.mapMarkerContainer}>
+                <Image style={styles.mapMarkerImage} source={{ uri: "https://images.unsplash.com/photo-1580913428023-02c695666d61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60" }} />
+                <Text style={styles.mapMarkerTitle}>Mercado</Text>
+              </View>
+            </Marker>
+
+          </MapView>
+        </View>
+      </View>
+      <View style={styles.itemsContainer}>
+        <ScrollView
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ paddingHorizontal: 20 }}
+        >
+          <TouchableOpacity style={styles.item} onPress={() => { }}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.181:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lampadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => { }}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.181:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lampadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => { }}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.181:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lampadas</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.item} onPress={() => { }}>
+            <SvgUri width={42} height={42} uri="http://192.168.1.181:3333/uploads/lampadas.svg" />
+            <Text style={styles.itemTitle}>Lampadas</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
+
+    </>
+  );
+}
+
+
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingHorizontal: 32,
-    //paddingTop: 20 + Constants.statusBarHeight,
+    paddingTop: 20 + Constants.statusBarHeight,
   },
 
   title: {
@@ -38,6 +120,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     overflow: 'hidden',
     marginTop: 16,
+
   },
 
   map: {
@@ -47,7 +130,7 @@ const styles = StyleSheet.create({
 
   mapMarker: {
     width: 90,
-    height: 80, 
+    height: 80,
   },
 
   mapMarkerContainer: {
@@ -108,3 +191,5 @@ const styles = StyleSheet.create({
     fontSize: 13,
   },
 });
+
+export default Points;
